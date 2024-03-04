574f3d16918ab0e0ed73496bbe61be61a93314a7
Alexandre Guz <guzalexandre@gmail.com>
2024-03-04 15:23:24 +0200

    opcao de escolher o teste com label normal

--- src/App.js
+++ src/App.js
@@ -5,29 +5,35 @@ function App() {
   const [selectedCommand, setSelectedCommand] = useState('');
   const [output, setOutput] = useState('');
 
-
-  const commands = [
-    'MIX_ENV=qa mix test test/regression/non_ui/ftp/upload_remessa_one_receivable_test.exs',
-    'MIX_ENV=qa mix test test/regression/open_sea/cadastro/credito_user_test.exs',
-    'Mais um Comando',
+  const commandOptions = [
+    { label: 'Upload Remessa FTP', value: 'MIX_ENV=qa mix test test/regression/non_ui/ftp/upload_remessa_one_receivable_test.exs' },
+    { label: 'Make Credit User', value: 'MIX_ENV=qa mix test test/regression/open_sea/cadastro/credito_user_test.exs' },
+    // Adicione mais opções conforme necessário
   ];
 
   const runCommand = async () => {
+    const selectedOption = commandOptions.find(option => option.label === selectedCommand);
+
+    if (!selectedOption) {
+      setOutput('Comando não encontrado.');
+      return;
+    }
+
+    const { value: selectedCommandValue } = selectedOption;
+
     try {
-      const response = await fetch(`http://localhost:3001/run-command?dir=${encodeURIComponent(directory)}&command=${encodeURIComponent(selectedCommand)}`);
+      const response = await fetch(`http://localhost:3001/run-command?dir=${encodeURIComponent(directory)}&command=${encodeURIComponent(selectedCommandValue)}`);
       const data = await response.text();
-      console.log("DATA: ", data);
-      setOutput(data);  // Atualiza o estado com a saída do comando
+      setOutput(data);
     } catch (error) {
       console.error('Erro ao enviar solicitação ao servidor:', error.message);
-      setOutput('Erro ao executar o comando');  // Atualiza o estado em caso de erro
+      setOutput('Erro ao executar o comando');
     }
   };
 
-
   return (
     <div>
-      <h1>Meu App React</h1>
+      <h1>Automation React App</h1>
       <label>
         Caminho do Diretório:
         <input
@@ -42,18 +48,18 @@ function App() {
         Selecione o Comando:
         <select value={selectedCommand} onChange={(e) => setSelectedCommand(e.target.value)}>
           <option value="">Selecione um Comando</option>
-          {commands.map((cmd, index) => (
-            <option key={index} value={cmd}>
-              {cmd}
+          {commandOptions.map((option, index) => (
+            <option key={index} value={option.label}>
+              {option.label}
             </option>
           ))}
         </select>
       </label>
+
       <br />
       <button onClick={runCommand}>Executar Comando</button>
       <br />
       <div>
-        {console.log("ESSE EH O OUTPUT", output)}
         <strong>Saída do Comando:</strong>
         <pre>{output}</pre>
       </div>
@@ -62,3 +68,5 @@ function App() {
 }
 
 export default App;
+
+
